0242df2a76c9af047a725df0fa6b13bf00023b51
Ефремов Олег <o.v.efremov@me.com>
2024-05-26 22:12:27 +0300

    Исправил большую ошибку.  Добавил свою модель User.

--- web_app/web_start/views.py
+++ web_app/web_start/views.py
@@ -1,9 +1,17 @@
+import logging
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from users.models import User
+
 
 def index(request):
+    users = User.objects.all()
+    for user in users:
+        logging.info(f'USER: {user.username}')
     context = {"is_authenticated": request.user.is_authenticated}
     return render(request, "index.html", context)
 
