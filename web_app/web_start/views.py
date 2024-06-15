034494de6dd392c974c39e7de9acaabfb5fd9009
Ефремов Олег <o.v.efremov@me.com>
2024-06-16 01:03:05 +0300

    Шаблоны, шаблоны и ещё раз шаблоны.  Проверка разных вариантов.

--- web_app/web_start/views.py
+++ web_app/web_start/views.py
@@ -9,10 +9,12 @@ from users.models import User
 
 
 def index(request):
-    context = {"user": request.user}
+    base_template = "base_messages_management.html" if request.user.is_authenticated else "base_overview_system.html"
+    context = {"base": base_template, "user": request.user, "title": "Тестовая страница"}
     return render(request, "index.html", context)
 
 
 def test(request):
-    context = {"user": request.user, "title": "Тестовая страница"}
-    return render(request, "base.html", context)
+    base_template = "base_messages_management.html" if request.user.is_authenticated else "base_overview_system.html"
+    context = {"base": base_template, "user": request.user, "title": "Тестовая страница"}
+    return render(request, "messages_management.html", context)
