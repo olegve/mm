5be122f270284a5507bebd00a3d1a9526bbd0f42
Ефремов Олег <o.v.efremov@me.com>
2024-07-22 17:40:18 +0300

    Revert "Не понял, что это"
    This reverts commit 29bde92ae8366983772bfc8438c3859f1ab725d3.

--- web_app/organizations/serializers 2.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from organizations.models import Organization
-
-
-class OrganizationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Organization
-        fields = "__all__"
